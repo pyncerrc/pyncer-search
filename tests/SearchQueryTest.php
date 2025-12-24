35df1ab66098641a641b5121cff28a22fce89a0b
pyncer <root@pyncer.com>
2025-12-24 06:40:10 -0500

    Added additional SearchQuery test.

--- tests/SearchQueryTest.php
+++ tests/SearchQueryTest.php
@@ -7,32 +7,27 @@ class SearchQueryTest extends TestCase
 {
     public function testSearchQuery(): void
     {
-        $query = new \Pyncer\Search\SearchQuery(
-            'a test case'
-        );
+        $query = new \Pyncer\Search\SearchQuery('a test case');
 
         $this->assertEquals($query->getQuery(), 'a test case');
         $this->assertTrue(count($query->getTerms()) === 1);
         $this->assertTrue($query->getTerms()[0]->getGroup() === true);
 
-        $query = new \Pyncer\Search\SearchQuery(
-            'a +test case'
-        );
+        $query = new \Pyncer\Search\SearchQuery('a +test case');
 
         $this->assertTrue(count($query->getTerms()) === 3);
 
-        $query = new \Pyncer\Search\SearchQuery(
-            'a -test case'
-        );
+        $query = new \Pyncer\Search\SearchQuery('a -test case');
 
         $this->assertTrue(count($query->getTerms()) === 3);
         $this->assertTrue($query->getTerms()[1]->getExclude() === true);
 
-        $query = new \Pyncer\Search\SearchQuery(
-            '"a test case"'
-        );
+        $query = new \Pyncer\Search\SearchQuery('"a test case"');
 
         $this->assertTrue(count($query->getTerms()) === 1);
         $this->assertTrue($query->getTerms()[0]->getPhrase() === true);
+
+        $query = new \Pyncer\Search\SearchQuery('');
+        $this->assertTrue(count($query->getTerms()) === 0);
     }
 }
